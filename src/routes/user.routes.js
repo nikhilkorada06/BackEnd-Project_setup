9097aad5d507f47b1af7b97a019c8386ee24a360
Nikhil Korada <nikhilkorada06@gmail.com>
2026-01-30 16:31:29 +0530

    Refactor user routes: add change password, current user retrieval, and user details update endpoints; enhance avatar and cover image update routes.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,21 @@
-import {Router} from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { Router } from "express";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
+import { verify } from "jsonwebtoken";
 
 const router = Router();
 
@@ -10,27 +24,37 @@ router.route("/register").post(
     //     console.log("Register route hit from user.routes.js");
     //     next();
     // },
-    
+
     upload.fields([
         {
             name: "avatar",
-            maxCount: 1
+            maxCount: 1,
         },
         {
             name: "coverImage",
-            maxCount: 1
-        }
+            maxCount: 1,
+        },
     ]),
     registerUser
 );
-                                                        //url - http://localhost:8000/api/v1/users/register
+//url - http://localhost:8000/api/v1/users/register
 // router.route("/login").post(login);                  //url - http://localhost:8000/api/v1/users/login
-
 
 router.route("/login").post(loginUser);
 
 //sercured routes...
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-user-details").patch(verifyJWT, updateUserDetails);
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
