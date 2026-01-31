6c0e372acecdbbc2f757ff66202f0b746e3d52f7
Nikhil Korada <nikhilkorada06@gmail.com>
2026-02-01 00:38:38 +0530

    feat: enhance video publishing functionality; add file upload validation and Cloudinary integration

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,41 +1,96 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiErrors.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Video } from "../models/video.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiErrors.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 import asyncHandler from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
+ 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
 })
 
+
+
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    
+    const { title, description } = req.body;
+
+    if(!title || !description) {
+        throw new ApiError(400, "Title and Description are required...💂🏻‍♀️💂🏻‍♀️💂🏻‍♀️");
+    }
+
+    if(!req.files || !req.files.videoFile || req.files.videoFile.length === 0 || !req.files.thumbnail || req.files.thumbnail.length === 0) {
+        throw new ApiError(400, "Video file and thumbnail are required...💂🏻‍♀️💂🏻‍♀️💂🏻‍♀️");
+    }
+
+    const videoFile = req.files?.videoFile[0];
+    const thumbnailFile = req.files?.thumbnail[0];
+
+    // Upload video to Cloudinary
+    const uploadedVideo = await uploadOnCloudinary(videoFile.path);
+    const uploadedThumbnail = await uploadOnCloudinary(thumbnailFile.path);
+
+    if(!uploadedVideo || !uploadedThumbnail) {  
+        throw new ApiError(500, "Error uploading files to Cloudinary...💂🏻‍♀️💂🏻‍♀️💂🏻‍♀️");
+    }
+
+    // Create video document in MongoDB
+    const newVideo = new Video({
+        videoFile: uploadedVideo.url,
+        thumbnail: uploadedThumbnail.url,
+        title,
+        description,
+        duration: uploadedVideo.duration,
+        owner: req.user._id
+    });
+
+    return res
+    .status(201)
+    .json ( new ApiResponse(
+            201, 
+            "Video Published Successfully...!!!🎉🎉🎉",
+            {
+                video: newVideo
+            }
+        )
+    );
+
 })
 
+
+
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    const { videoId } = req.params;
     //TODO: get video by id
 })
 
+
+
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    const { videoId } = req.params;
     //TODO: update video details like title, description, thumbnail
 
 })
 
+
+
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    const { videoId } = req.params;
     //TODO: delete video
 })
 
+
+
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    const { videoId } = req.params;
 })
+
+
 
 export {
     getAllVideos,
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -7,36 +7,37 @@ import {
     togglePublishStatus,
     updateVideo,
 } from "../controllers/video.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
-import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use( verifyJWT ); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
-    .get(getAllVideos)
+    .get( getAllVideos )
     .post(
-        upload.fields([
-            {
-                name: "videoFile",
-                maxCount: 1,
-            },
-            {
-                name: "thumbnail",
-                maxCount: 1,
-            },
-            
-        ]),
+        upload.fields(
+            [
+                {
+                    name: "videoFile",
+                    maxCount: 1,
+                },
+                {
+                    name: "thumbnail",
+                    maxCount: 1,
+                },
+            ]
+        ),
         publishAVideo
     );
 
 router
-    .route("/:videoId")
-    .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.single("thumbnail"), updateVideo);
+    .route( "/:videoId" )
+    .get( getVideoById )
+    .delete( deleteVideo )
+    .patch( upload.single("thumbnail"), updateVideo );
 
-router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+router.route( "/toggle/publish/:videoId" ).patch( togglePublishStatus );
 
 export default router
